04b4ea562f762858badc479e624db056c3b21baf
Vini Avena <admin@admins-MacBook-Air.local>
2021-05-10 19:10:32 -0300

    access my statement by my cpf in the header

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const { request, response } = require('express');
 const express = require('express');
 const { v4: uuidv4 } = require('uuid');
 
@@ -30,6 +31,16 @@ app.post('/account', (request, response) => {
     statement: [],
   });
   return response.status(201).send();
+});
+
+app.get('/statement', (request, response) => {
+  const { cpf } = request.headers;
+  const customer = customers.find(customer => customer.cpf === cpf);
+  if (!customer) {
+    return response.status(400).json({ error: 'Conta não existente' });
+  }
+
+  return response.json(customer.statement);
 });
 
 app.listen(3333);
