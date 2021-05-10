dfb2a851be97b15dfaf35ed4abccae92a49c11a2
Vini Avena <admin@admins-MacBook-Air.local>
2021-05-10 19:51:20 -0300

    using middlewares for user validation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-const { request, response } = require('express');
 const express = require('express');
 const { v4: uuidv4 } = require('uuid');
 
@@ -7,6 +6,18 @@ const app = express();
 app.use(express.json());
 
 const customers = [];
+
+//Middleware
+function verifyExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+  const customer = customers.find(customer => customer.cpf === cpf);
+  if (!customer) {
+    return response.status(400).json({ error: 'Conta não existente' });
+  }
+  request.customer = customer;
+
+  return next();
+}
 
 // Criar a conta
 /*
@@ -17,9 +28,7 @@ const customers = [];
  */
 app.post('/account', (request, response) => {
   const { cpf, name } = request.body;
-
   const cpfExisted = customers.some(customer => customer.cpf === cpf);
-
   if (cpfExisted) {
     return response.status(400).json({ error: 'Cpf já utilizado' });
   }
@@ -30,16 +39,16 @@ app.post('/account', (request, response) => {
     id: uuidv4(),
     statement: [],
   });
-  return response.status(201).send();
+  return response.status(201).json({
+    message: 'Conta criada com sucesso',
+    name: name,
+    cpf: cpf,
+    id: customers.id,
+  });
 });
 
-app.get('/statement', (request, response) => {
-  const { cpf } = request.headers;
-  const customer = customers.find(customer => customer.cpf === cpf);
-  if (!customer) {
-    return response.status(400).json({ error: 'Conta não existente' });
-  }
-
+app.get('/statement', verifyExistsAccountCPF, (request, response) => {
+  const { customer } = request;
   return response.json(customer.statement);
 });
 
